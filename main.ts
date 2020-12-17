3ec36c05fcf70cb31d1c057b3f6ab935eec0c02e
gtg079x <67651413+gtg079x@users.noreply.github.com>
2020-12-16 19:45:49 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,41 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (receivedNumber == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (receivedNumber == 8) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
